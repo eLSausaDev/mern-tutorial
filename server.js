88baa5475ee0981076ebd6bdacae7e0831816fae
elsausadev <elsausadev@gmail.com>
2024-04-25 08:04:08 +0800

    Go22

--- server.js
+++ server.js
@@ -2,7 +2,7 @@ const express = require('express')
 const app = express()
 const path = require('path')
 const PORT = process.env.PORT || 3500
-
+//5555
 app.use('/', express.static(path.join(__dirname, '/public')))
 
 app.use('/', require('./routes/root'))
