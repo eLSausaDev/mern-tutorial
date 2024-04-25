1893d43c68a67ac8b4bef4b245b78b724d2b2033
elsausadev <elsausadev@gmail.com>
2024-04-25 20:35:17 +0800

    Ho2234

--- middleware/logger.js
+++ middleware/logger.js
@@ -3,5 +3,25 @@ const { v4: uuid } = require('uuid')
 const fs = require('fs')
 const fsPromises = require('fs').promises
 const path = require('path')
+const { Z_ASCII } = require('zlib')
 
-const logEvents
+const logEvents = async (message, logFileName) => {
+    const dateTime = `${format(new Date(), 'yyyyMMdd\tHH:mm:ss')}`
+    const logItem = `${dateTime}\t${uuid()}\t${message}\n`
+
+    try {
+        if (!fs.existsSync(path.join(__dirname, '..',  'logs'))) {
+            await fsPromises.mkdir(path.join(__dirname, '..', 'logs'))
+        }
+        await fsPromises.appendFile(path.join(__dirname, '..', 'logs', logFileName), logItem)
+    } catch (err) {
+        console.log(err)
+    }
+}
+
+const logger = (req, res, next) => {
+    logEvents(`${req.method}\t${req.url}\t${req.headers.origin}`, 'reqLog.log')
+    console.log(`${req.method} ${req.path}`)
+}
+
+module.exports = { logEvents, logger }
